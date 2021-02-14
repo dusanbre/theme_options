f7b54dc32e8ca6d7f9003e55d1390df1c28c7ab5
dusanbre <dusan.antonijevic2404@gmail.com>
2021-02-14 11:35:49 +0100

    color picker component in progress

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,36 +1,54 @@
 import { SketchPicker } from "react-color";
 const { useState, Fragment } = wp.element;
 
-export default function ColorPicker({ keyName }) {
+import "../scss/color-picker.scss";
+
+export default function ColorPicker({ keyIndex, keyName, title }) {
   const [color, setColor] = useState({});
   const [visible, setVisible] = useState(false);
-  console.log(color[keyName]);
+  const [brightness, setBrightness] = useState(false);
+  // console.log(color[keyName]);
+
+  const checkHexLight = (color) => {
+    const hex = color.replace("#", "");
+    const c_r = parseInt(hex.substr(0, 2), 16);
+    const c_g = parseInt(hex.substr(2, 2), 16);
+    const c_b = parseInt(hex.substr(4, 2), 16);
+    const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
+    return brightness > 155;
+  };
+
   return (
-    <Fragment>
+    <div className="anps-color-pick-wrap">
+      <h4>{title}</h4>
       <div
         className="anps-color-pick"
         onClick={() => {
           setVisible(!visible);
         }}
         style={{
-          width: 70,
-          height: 30,
           background: Object.keys(color).length > 0 ? color[keyName] : "black",
-          color: "white",
+          color: brightness ? "black" : "white",
         }}
       >
-        {color.keyName}
+        {color[keyName]}
       </div>
+      <i className="fa fa-times"></i>
+
       {visible ? (
         <SketchPicker
           color={color[keyName]}
           onChange={(value) => {
             setColor({ [keyName]: value.hex });
           }}
+          onChangeComplete={(value) => {
+            const bright = checkHexLight(value.hex);
+            setBrightness(bright);
+          }}
         />
       ) : (
         ""
       )}
-    </Fragment>
+    </div>
   );
 }
--- src/pages/ThemeColor.js
+++ src/pages/ThemeColor.js
@@ -5,9 +5,12 @@ export default function ThemeColor() {
   const [color, setColor] = useState();
   return (
     <div className="admin-modal">
-      <div className="admin-modal__title"></div>
+      <h3 className="admin-modal__title">
+        <i class="fa fa-tint"></i>Main Theme Color
+      </h3>
       <div className="admin-modal__content">
-        <ColorPicker keyName={"colortheme"} />
+        <ColorPicker keyName="colortheme" title="Text Color" />
+        <ColorPicker keyName="colortheme" title="Content Color" />
       </div>
     </div>
   );
